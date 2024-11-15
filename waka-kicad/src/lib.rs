4d96f28078cf520c570cd70c763d7ab5979ea387
sporeball <sporeballdev@gmail.com>
2024-11-15 12:35:31 -0500

    turn kicad crate back on

--- waka-kicad/src/lib.rs
+++ waka-kicad/src/lib.rs
@@ -1,3 +1,4 @@
+// use kicad::{DocumentType, KiCad};
 use sysinfo::{Pid, Process, System};
 
 pub trait FindProcess {
@@ -8,7 +9,7 @@ impl FindProcess for System {
   fn find_process(&self, name: &str) -> Option<(&Pid, &Process)> {
     self.processes()
       .iter()
-      .filter(|(pid, process)| process.exe().is_some_and(|e| e.ends_with(name)))
+      .filter(|(_pid, process)| process.exe().is_some_and(|e| e.ends_with(name)))
       .collect::<Vec<_>>()
       .first()
       .cloned()
--- waka-kicad/src/main.rs
+++ waka-kicad/src/main.rs
@@ -1,26 +1,32 @@
-use waka_kicad::FindProcess;
-// use kicad::{KiCad, KiCadConnectionConfig};
+// use waka_kicad::FindProcess;
+// only works on KiCAD nightly
+use kicad::{DocumentType, KiCad, KiCadConnectionConfig};
 use sysinfo::System;
 
-// fn main() -> Result<(), anyhow::Error> {
 fn main() -> Result<(), anyhow::Error> {
-  // TODO: blocked by the GitLab issue i submitted
-  // let k = KiCad::new(KiCadConnectionConfig {
-  //   client_name: String::from("waka-kicad"),
-  //   ..Default::default()
-  // }).expect("KiCAD not running!");
-  // println!("Connected to KiCAD {}", k.get_version().unwrap());
+  // TODO: wait instead of expect
+  let k = KiCad::new(KiCadConnectionConfig {
+    client_name: String::from("waka-kicad"),
+    ..Default::default()
+  }).expect("KiCAD not running!");
+  println!("Connected to KiCAD {}", k.get_version().unwrap());
   let mut sys = System::new_all();
   sys.refresh_all();
-  // for (pid, process) in sys.processes() {
-  //   println!("[{pid}] {:?} {:?}", process.name(), process.exe());
-  // }
   // TODO: inaccurate? vvv
-  let kicads = sys.processes_by_name("kicad".as_ref());
-  let schematic_editors = sys.processes_by_name("eeschema".as_ref());
-  let pcb_editors = sys.processes_by_name("pcbnew".as_ref());
-  println!("{} instances of KiCad open", kicads.count());
-  println!("{} schematic editors open", schematic_editors.count());
-  println!("{} pcb editors open", pcb_editors.count());
+  // let kicads = sys.processes_by_name("kicad".as_ref());
+  // let schematic_editors = sys.processes_by_name("eeschema".as_ref());
+  // let pcb_editors = sys.processes_by_name("pcbnew".as_ref());
+  // println!("{} instances of KiCad open", kicads.count());
+  // println!("{} schematic editors open", schematic_editors.count());
+  // println!("{} pcb editors open", pcb_editors.count());
+  if let Ok(schematics) = k.get_open_documents(DocumentType::DOCTYPE_SCHEMATIC) {
+    println!("Found {} open schematic(s)", schematics.len());
+  }
+  if let Ok(pcbs) = k.get_open_documents(DocumentType::DOCTYPE_PCB) {
+    println!("Found {} open PCB(s)", pcbs.len());
+  }
+  if let Ok(board) = k.get_open_board() {
+    println!("Found open board: {:?}", board);
+  }
   Ok(())
 }
