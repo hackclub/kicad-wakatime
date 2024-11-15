e9647d034d1dd9593d6bcc883dce40b13c8646e6
sporeball <sporeballdev@gmail.com>
2024-11-15 13:37:12 -0500

    add env_logger crate

--- waka-kicad/src/main.rs
+++ waka-kicad/src/main.rs
@@ -1,15 +1,17 @@
 // use waka_kicad::FindProcess;
 // only works on KiCAD nightly
 use kicad::{DocumentType, KiCad, KiCadConnectionConfig};
+use log::{debug, error, info, LevelFilter};
 use sysinfo::System;
 
 fn main() -> Result<(), anyhow::Error> {
+  env_logger::init();
   // TODO: wait instead of expect
   let k = KiCad::new(KiCadConnectionConfig {
     client_name: String::from("waka-kicad"),
     ..Default::default()
   }).expect("KiCAD not running!");
-  println!("Connected to KiCAD {}", k.get_version().unwrap());
+  info!("Connected to KiCAD {}", k.get_version().unwrap());
   let mut sys = System::new_all();
   sys.refresh_all();
   // TODO: inaccurate? vvv
@@ -20,13 +22,13 @@ fn main() -> Result<(), anyhow::Error> {
   // println!("{} schematic editors open", schematic_editors.count());
   // println!("{} pcb editors open", pcb_editors.count());
   if let Ok(schematics) = k.get_open_documents(DocumentType::DOCTYPE_SCHEMATIC) {
-    println!("Found {} open schematic(s)", schematics.len());
+    info!("Found {} open schematic(s)", schematics.len());
   }
   if let Ok(pcbs) = k.get_open_documents(DocumentType::DOCTYPE_PCB) {
-    println!("Found {} open PCB(s)", pcbs.len());
+    info!("Found {} open PCB(s)", pcbs.len());
   }
   if let Ok(board) = k.get_open_board() {
-    println!("Found open board: {:?}", board);
+    info!("Found open board: {:?}", board);
   }
   Ok(())
 }
