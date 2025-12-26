0e2f0ab65b1643ca16d6c656bb959c7d7745d81a
sporeball <sporeballdev@gmail.com>
2025-12-26 14:34:18 -0500

    0.3.0
    the installation video has been removed at my request

--- script.js
+++ script.js
@@ -9,20 +9,20 @@ const kicad_wakatime_version = document.getElementById('kicad-wakatime-version')
 
 const e_download_kicad = document.getElementById('download-kicad');
 const e_download_kicad_wakatime = document.getElementById('download-kicad-wakatime');
-const e_download_kicad_href = os == 'windows' ? 'https://downloads.kicad.org/kicad/windows/explore/nightlies'
-  : os == 'macos' ? 'https://downloads.kicad.org/kicad/macos/explore/nightlies'
+const e_download_kicad_href = os == 'windows' ? 'https://www.kicad.org/download/windows'
+  : os == 'macos' ? 'https://www.kicad.org/download/macos'
   : os == 'linux' ? 'https://www.kicad.org/download/linux'
   : null;
-const e_download_kicad_wakatime_href = os == 'windows' ? 'https://github.com/hackclub/kicad-wakatime/releases/download/0.2.1/kicad-wakatime-0.2.1-windows.zip'
-  : os == 'macos' ? 'https://github.com/hackclub/kicad-wakatime/releases/download/0.2.1/kicad-wakatime-0.2.1-macos.zip'
-  : os == 'linux' ? 'https://github.com/hackclub/kicad-wakatime/releases/download/0.2.1/kicad-wakatime-0.2.1-linux.zip'
-  : 'https://github.com/hackclub/kicad-wakatime/releases/tag/0.2.1'
+const e_download_kicad_wakatime_href = os == 'windows' ? 'https://github.com/hackclub/kicad-wakatime/releases/download/0.3.0/kicad-wakatime-0.3.0-windows.zip'
+  : os == 'macos' ? 'https://github.com/hackclub/kicad-wakatime/releases/download/0.3.0/kicad-wakatime-0.3.0-macos.zip'
+  : os == 'linux' ? 'https://github.com/hackclub/kicad-wakatime/releases/download/0.3.0/kicad-wakatime-0.3.0-linux.zip'
+  : 'https://github.com/hackclub/kicad-wakatime/releases/tag/0.3.0'
 
 document.getElementById('os').innerHTML = os;
 
-e_download_kicad.innerHTML = os == 'windows' ? `<a href="${e_download_kicad_href}" target="_blank">download KiCAD 8.99 (.exe)</a>`
-  : os == 'macos' ? `<a href="${e_download_kicad_href}" target="_blank">download KiCAD 8.99 (.dmg)</a>`
-  : os == 'linux' ? `<a href="${e_download_kicad_href}" target="_blank">download KiCAD 8.99 (instructions)</a>`
-  : `download KiCAD 8.99: <a href="https://www.kicad.org/download/linux" target="_blank">Linux</a> or <a href="https://downloads.kicad.org/kicad" target="_blank">not Linux</a>`;
+e_download_kicad.innerHTML = os == 'windows' ? `<a href="${e_download_kicad_href}" target="_blank">download KiCAD (.exe)</a>`
+  : os == 'macos' ? `<a href="${e_download_kicad_href}" target="_blank">download KiCAD (.dmg)</a>`
+  : os == 'linux' ? `<a href="${e_download_kicad_href}" target="_blank">download KiCAD (instructions)</a>`
+  : `<a href="https://www.kicad.org/download" target="_blank">download KiCAD</a>`;
 
 e_download_kicad_wakatime.innerHTML = `<a href="${e_download_kicad_wakatime_href}" target="_blank">download kicad-wakatime (.zip)</a>`;
